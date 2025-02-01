0c473caf1e93def9380e258c2153206381b4bb5c
chieftonyit <chieftonyit1985@gmail.com>
2025-01-31 20:26:51 -0600

    repository secret

---
+++ main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "Action-VPC" {
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "Actions-VPC"
+    env  = "prod"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "buck-git-terra-action"
+    key    = "gitactions/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
