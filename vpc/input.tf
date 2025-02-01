2eb7d3c538be253ae939ce2105d315f406cc9c16
chieftonyit <chieftonyit1985@gmail.com>
2025-02-01 17:53:08 -0600

    fix: provide input

--- vpc/input.tf
+++ vpc/input.tf
@@ -1,19 +1,15 @@
 variable "region" {
   type = string
-  default = "us-east-1"
 }
 
 variable "ciblk" {
   type = string
-  default = "10.0.0.0/24"
 }
 
 variable "name" {
   type = string
-  default = "Tykama-VPC"
 }
 
 variable "env" {
   type = string
-  default = "dev"
 }
